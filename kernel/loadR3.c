2ad0031201ee73da395959e4ea971b0617a1aae1
Wanlace Yates <wcy00001@mix.wvu.edu>
2023-03-30 14:56:56 -0400

    Post-R3/R4: Change class of loadR3 processes to USER

--- kernel/loadR3.c
+++ kernel/loadR3.c
@@ -7,24 +7,23 @@
 #include <stdint.h>
 
 int loadR3() {
-    struct pcb* pro1 = pcb_setup("proc1", KERNEL, 1);
+    struct pcb* pro1 = pcb_setup("proc1", USER, 1);
     pcb_context_init(pro1, proc1, NULL, 0);
     pcb_insert(pro1);
 
-    struct pcb* pro2 = pcb_setup("proc2", KERNEL, 1);
+    struct pcb* pro2 = pcb_setup("proc2", USER, 1);
     pcb_context_init(pro2, proc2, NULL, 0);
     pcb_insert(pro2);
 
-    struct pcb* pro3 = pcb_setup("proc3", KERNEL, 1);
+    struct pcb* pro3 = pcb_setup("proc3", USER, 1);
     pcb_context_init(pro3, proc3, NULL, 0);
     pcb_insert(pro3);
 
-
-    struct pcb* pro4 = pcb_setup("proc4", KERNEL, 1);
+    struct pcb* pro4 = pcb_setup("proc4", USER, 1);
     pcb_context_init(pro4, proc4, NULL, 0);
     pcb_insert(pro4);
 
-    struct pcb* pro5 = pcb_setup("proc5", KERNEL, 1);
+    struct pcb* pro5 = pcb_setup("proc5", USER, 1);
     pcb_context_init(pro5, proc5, NULL, 0);
     pcb_insert(pro5);
     
